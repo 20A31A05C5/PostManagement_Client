7e3515a0faf55afa83eabaa1be1e73972bb364bd
Satish Somala <109905044+20A31A05C5@users.noreply.github.com>
2025-06-28 10:04:29 +0530

    Update News.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,6 +7,7 @@ import Ct from './Ct'
 function News() {
   let [data,setData]=useState([])
   let [f,setF]=useState(true)
+  const baseUrl = process.env.REACT_APP_API_URL?.replace(/\/+$/, '');
   let navigate=useNavigate()
   let [cm,setCm]=useState({})
   let [acom,setAcom]=useState({})
@@ -18,7 +19,7 @@ function News() {
        obj.updStore(t)
     }
     
-    axios.get(`${process.env.REACT_APP_API_URL}/getcat/news`).then((res)=>{
+    axios.get(`${baseUrl}/getcat/news`).then((res)=>{
       setData(res.data)
     })
   },[f])
@@ -26,7 +27,7 @@ function News() {
     let t=Cookies.get("lgc")
     if(t!=undefined){
       t=JSON.parse(t)
-      axios.put(`${process.env.REACT_APP_API_URL}/like`,{"pid":pid,"uid":t.uid}).then((res)=>{
+      axios.put(`${baseUrl}/like`,{"pid":pid,"uid":t.uid}).then((res)=>{
         setF(!f)
       })
     }
@@ -38,7 +39,7 @@ function News() {
     let t=Cookies.get("lgc")
     if(t!=undefined){
       t=JSON.parse(t)
-      axios.put(`${process.env.REACT_APP_API_URL}/dlike`,{"pid":pid,"uid":t.uid}).then((res)=>{
+      axios.put(`${baseUrl}/dlike`,{"pid":pid,"uid":t.uid}).then((res)=>{
         setF(!f)
       })
     }
@@ -54,14 +55,14 @@ function News() {
     .join(" ");
   }
   let addComment=(pid)=>{
-    axios.put(`${process.env.REACT_APP_API_URL}/addcomm`,{"pid":pid,"txt":acom[pid],"name":obj.store.name,"uid":obj.store.uid}).then((res)=>{
+    axios.put(`${baseUrl}/addcomm`,{"pid":pid,"txt":acom[pid],"name":obj.store.name,"uid":obj.store.uid}).then((res)=>{
       setAcom({...acom,[pid]:""})
 
       setF(!f)
     })
   }
   let delcom=(pid,cid)=>{
-    axios.put(`${process.env.REACT_APP_API_URL}/delcom`,{"pid":pid,"cid":cid}).then((res)=>{
+    axios.put(`${baseUrl}/delcom`,{"pid":pid,"cid":cid}).then((res)=>{
       setF(!f)
     })
   }
